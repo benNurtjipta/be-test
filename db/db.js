191349e28a69d22089219ff0be787bef4976f887
benNurtjipta <ben@nurtjipta.de>
2025-07-02 11:18:26 +0200

    basic route

---
+++ db/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+export default {
+  connect: async () => {
+    mongoose.connection.on("connected", () => console.log(`connected to DB`));
+    mongoose.connection.on("disconnected", () =>
+      console.log(`disconnected from DB`)
+    );
+    mongoose.connection.on("error", (error) => console.log(`DB error`, error));
+
+    await mongoose.connect(process.env.MONGO_DB_URI);
+  },
+  disconnect: async () => {
+    await mongoose.disconnect();
+  },
+};
