cb58b1b25749383cce633c934fd60257699af1ab
MrMister20257 <norman.a.marin@gmail.com>
2025-07-02 22:42:11 +0200

    Füge Funktion zum Abrufen aller Anzeigen hinzu und aktualisiere die Benutzerroute

---
+++ controllers/getAllAds.js
@@ -0,0 +1,24 @@
+import Anzeigen from "../models/AnzeigenModel.js"
+
+
+export const getAllAds = async (req, res, next) => {
+    try {
+        const ads = await Anzeigen.find({})
+        .select("-appliedFrom -__v")
+        .populate({
+            path: "createdFrom",
+            select: "-hashedPassword -email -createdAt -role -adsCreated -__v"
+        })
+        .sort({ createdAt: -1})
+
+        if (!ads || ads.length === 0) {
+            const error = new Error("Keine Anzeigen gefunden");
+            error.statusCode = 404
+        }
+
+        return res.status(200).json(ads)
+
+    } catch (error) {
+        return next(error)
+    }
+}
