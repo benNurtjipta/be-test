64410173e17232a5d7328b8f22b5d1bacd847495
benNurtjipta <ben@nurtjipta.de>
2025-07-02 11:34:41 +0200

    userrouter

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,4 +2,10 @@ import express from "express";
 
 const userRouter = express.Router();
 
+userRouter.get("/ad");
+
+userRouter.post("/ad/:id");
+
+userRouter.post("/create");
+
 export default userRouter;
